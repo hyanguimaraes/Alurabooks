4692a2101f200d1dee8d6fdf06915a0446e647fb
hyanguimaraes <hyangt@hotmail.com>
2023-01-22 16:57:05 -0300

    Carrossel finalizado.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//MENU LATERAL
 const botaoMenu = document.querySelector('.cabecalho_menu_hamburguer');
 const menu = document.querySelector('.lista_menu');
 
@@ -5,3 +6,13 @@ botaoMenu.addEventListener('click', ()=> {
     menu.classList.toggle('lista_menu_ativo');
     botaoMenu.classList.toggle('cabecalho_menu_hamburguer_ativo')
 });
+
+//SWIPER
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+  });
