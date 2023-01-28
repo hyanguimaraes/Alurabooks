1a843349f5245162a8bb19e815f4a311963bb5e7
hyanguimaraes <hyangt@hotmail.com>
2023-01-28 10:01:57 -0300

    Finalizada a versão mobile do site AluraBooks.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 //MENU LATERAL
 const botaoMenu = document.querySelector('.cabecalho_menu_hamburguer');
 const menu = document.querySelector('.lista_menu');
+const paginacao = document.querySelector('.swiper-pagination')
 
 botaoMenu.addEventListener('click', ()=> {
     menu.classList.toggle('lista_menu_ativo');
     botaoMenu.classList.toggle('cabecalho_menu_hamburguer_ativo')
+    paginacao.classList.toggle('swiper-pagination_menu_ativo')
 });
 
 //SWIPER
